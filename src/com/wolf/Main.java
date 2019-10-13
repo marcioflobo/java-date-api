1f0a5340a72ded579fb5ef7e74d9f92df86dc9c5
Márcio Lôbo <marcioloboti@gmail.com>
2019-10-13 16:57:37 -0300

    Manipulating dates with Java Calendar

--- src/com/wolf/Main.java
+++ src/com/wolf/Main.java
@@ -3,8 +3,8 @@ package com.wolf;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 
 
@@ -12,60 +12,27 @@ public class Main {
 
 	public static void main(String[] args) throws ParseException {
 		
-		//Create a simple date format
-		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
-		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		//Summing a unit of time with Java Calendar
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 		
-		//Setting time zone to GMT
-		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+		System.out.println(sdf.format(d));
 		
-		//Assigns the current time instant to a variable
-		Date x1 = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		d = cal.getTime();
+		System.out.println(sdf.format(d));
 		
-		//Alternate form to Assigns the current time instant to a variable  
-		Date x2 = new Date(System.currentTimeMillis());
+		//Getting a unit of time
 		
-		//Java default date
-		Date x3 = new Date(0L);
-		
-		Date x4 = new Date(1000L * 60L * 60L * 5L);
-		
-		Date y1 = sdf1.parse("25/06/2018");
-		Date y2 = sdf2.parse("25/06/2018 15:42:07");
-		
-		//ISO 8601 UTC Patterns
-		Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		int minutes = cal.get(Calendar.MINUTE);
+		int month = 1 + cal.get(Calendar.MONTH);
+		System.out.println("Minutes: " + minutes);
+		System.out.println("Month: " + month);
 		
 		
-		System.out.println("Default Java date Print");
-		System.out.println("x1: " + x1);
-		System.out.println("x2: " + x2);
-		System.out.println("x3: " + x3);
-		System.out.println("x4: " + x4);
-		System.out.println("y1: " + y1);
-		System.out.println("y2: " + y2);
-		System.out.println("y3: " + y3);
-		System.out.println();
 		
-		System.out.println("With custom format");
-		System.out.println("x1: " + sdf2.format(x1));
-		System.out.println("x2: " + sdf2.format(x2));
-		System.out.println("x3: " + sdf2.format(x3));
-		System.out.println("x4: " + sdf2.format(x4));
-		System.out.println("y1: " + sdf2.format(y1));
-		System.out.println("y2: " + sdf2.format(y2));
-		System.out.println("y3: " + sdf2.format(y3));
-		System.out.println();
-		
-		System.out.println("With time zone configured");
-		System.out.println("x1: " + sdf3.format(x1));
-		System.out.println("x2: " + sdf3.format(x2));
-		System.out.println("x3: " + sdf3.format(x3));
-		System.out.println("x4: " + sdf3.format(x4));
-		System.out.println("y1: " + sdf3.format(y1));
-		System.out.println("y2: " + sdf3.format(y2));
-		System.out.println("y3: " + sdf3.format(y3));
 	}
 
 }
